69916432aaecff797926bc804114c1d10eabd734
stevan dedovic <stevan dedovic>
2016-08-08 20:34:39 -0500

    add debuging, utils

--- src/com/dedovic/Main.java
+++ src/com/dedovic/Main.java
@@ -2,17 +2,37 @@ package com.dedovic;
 
 import com.dedovic.midi.MidiDeviceConfiguration;
 import com.dedovic.midi.MidiLaunchpad;
-import com.dedovic.midi.component.MidiComponent;
-import com.dedovic.midi.component.impl.MidiDumpComponent;
-import com.dedovic.midi.component.impl.RandomVelocityComponent;
+import com.dedovic.midi.MidiUtils;
 import com.dedovic.midi.controller.impl.SimpleController;
 
 import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 
 public class Main {
+    private static final boolean DEBUG = true;
+
     public static void main(String[] args) {
+        if (DEBUG) {
+            try {
+                System.out.println("Devices:");
+                for(MidiDevice.Info deviceInfo: MidiSystem.getMidiDeviceInfo()) {
+                    String output = deviceInfo.getName();
+                    MidiDevice device = MidiSystem.getMidiDevice(deviceInfo);
+
+                    output += device.getMaxTransmitters() == -1 ? ", Reciever" : "";
+                    output += device.getMaxReceivers() == -1 ? ", Transmitter" : "";
+
+                    System.out.println(output);
+                }
+            } catch (MidiUnavailableException e) {
+                e.printStackTrace();
+            }
+        }
         try {
+
+            MidiDevice drums = MidiUtils.getTransmitterByName("E-MU XMidi1X1 ");
+
             MidiLaunchpad launchpad = new MidiLaunchpad(MidiDeviceConfiguration.autodetect());
             MidiDevice controller = new SimpleController();
 
--- src/com/dedovic/midi/MidiUtils.java
+++ src/com/dedovic/midi/MidiUtils.java
@@ -1,21 +1,47 @@
 package com.dedovic.midi;
 
-import java.util.HashMap;
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MidiUtils {
 
     public static final String[] KEYS = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
     public static String keyToString(int key) {
-        if (key > 127)
-        {
+        if (key > 127) {
             return "illegal value";
-        }
-        else
-        {
-            int	nNote = key % 12;
-            int	nOctave = key / 12;
+        } else {
+            int nNote = key % 12;
+            int nOctave = key / 12;
             return KEYS[nNote] + (nOctave - 1) + "(" + key + ")";
         }
+    }
+
+    public static List<MidiDevice> getDevicesByName(String deviceName) throws MidiUnavailableException {
+        List<MidiDevice> out = new ArrayList<>();
+
+        MidiDevice.Info[] midiDeviceInfo = MidiSystem.getMidiDeviceInfo();
+        for (MidiDevice.Info info : midiDeviceInfo) {
+            if (info.getName().equals(deviceName)) {
+                out.add(MidiSystem.getMidiDevice(info));
+            }
+        }
+        return out;
+    }
+
+    public static MidiDevice getTransmitterByName(String name) {
+        try {
+            return getDevicesByName(name).stream()
+                    .filter(device -> device.getMaxReceivers() == -1)
+                    .collect(Collectors.toList())
+                    .get(0);
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
